d82a816d8882a5fdf977b6bc6d7016c94eb4045e
matusrebro <matus.rebro@tutanota.com>
2021-09-26 16:21:41 +0200

    basal metabolic rate prediction model added

--- simulations.py
+++ simulations.py
@@ -7,13 +7,16 @@ import pandas as pd
 from Diabetes import minimal_model
 from glucose_metabolism import glucose_metabolism_app
 from adaptive_control import adaptive_control_app
+from body_mass_sim import body_mass_app
 
 
 def app():
     st.title("Simulators of dynamic systems")
-    selections = ["Glucose metabolism", "Adaptive control for T1DM"]
+    selections = ["Glucose metabolism", "Adaptive control for T1DM", "Body mass change"]
     simulationSelection = st.selectbox("Simulation", options=selections)
     if simulationSelection == "Glucose metabolism":
         glucose_metabolism_app()
     elif simulationSelection == "Adaptive control for T1DM":
         adaptive_control_app()
+    elif simulationSelection == "Body mass change":
+        body_mass_app()
